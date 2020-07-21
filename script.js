d3bcf3902c22fccdbfdf3821b8f1c2aea35f49b2
Kira <kira@dogmail.com>
2020-07-21 19:40:55 -0400

    updating.v2

---
+++ script.js
@@ -0,0 +1,19 @@
+$("#find-city").on("click", function(event) {
+
+    event.preventDefault();
+
+    // Here we grab the text from the input box
+    var city = $("#city-input").val();
+    // api key
+    var key = "540c1512d41801eb53d62f05f55a02d0";
+
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + key;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        $(".current-city").text(JSON.stringify(response));
+    });
+
+});
