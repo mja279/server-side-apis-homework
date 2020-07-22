d1836ddba9e44adeaff93f5ab0b070a196bc6c52
Kira <kira@dogmail.com>
2020-07-21 21:48:35 -0400

    updating.v3

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # server-side-apis-homework
 Weather Dashboard 
+
+<img src="">
+<img src="">
+
+Weather Widgets source from: https://codepen.io/EvanWieland/pen/PwOKLE 
+
+Submission:
+
+The URL of the functional, deployed application: 
+
+The URL of the GitHub repository:
--- script.js
+++ script.js
@@ -8,12 +8,12 @@ $("#find-city").on("click", function(event) {
     var key = "540c1512d41801eb53d62f05f55a02d0";
 
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + key;
-
+    
     $.ajax({
         url: queryURL,
         method: "GET"
       }).then(function(response) {
-        $(".current-city").text(JSON.stringify(response));
+        $("#city-view").text(JSON.stringify(response));
     });
 
 });
